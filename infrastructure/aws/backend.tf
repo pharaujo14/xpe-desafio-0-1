15731fe9387694bf33b54f410dc8fb965e556585
Paulo Henrique Pereira <pharaujo14@gmail.com>
2023-05-30 20:28:35 -0300

    Ajustes

--- infrastructure/aws/backend.tf
+++
@@ -1,8 +0,0 @@
-# Backend configuration require a AWS storage bucket
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-igti-paulo"
-    key    = "state/igti/edc/mod1/desafio_final_v2/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
