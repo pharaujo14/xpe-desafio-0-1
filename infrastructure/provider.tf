a0fd24edde92603bba0671458b69c92c1d3b3b5a
Paulo Henrique Pereira <pharaujo14@gmail.com>
2023-05-30 21:27:00 -0300

    Ajustes no deploy com terraform

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region  = var.aws_region
+  region  = "us-east-2"
   version = "~> 3.37"
 }
+
 
 terraform {
   backend "s3" {
     bucket = "terraform-state-igti-paulo"
-    region = var.aws_region
+    region = "us-east-2"
   }
 }
